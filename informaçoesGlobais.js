f86bddb1931c0495e48f661bfff89bf0d1ebcd9c
xaulindematadordeporco <106355661+xaulindematadordeporco@users.noreply.github.com>
2024-10-15 22:52:11 -0300

    resgerg

--- informaçoesGlobais.js
+++ informaçoesGlobais.js
@@ -5,6 +5,12 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = res.json();
     console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-conteiner__texto')
+    paragrafo.innerHTML = 'voce sabia que o mundo tem &{dados.
+    total_pessoas_mundo} e que aproximamente &{dados.
+    total_pessoas_conectadas} estao conectadas em alguma rede social e passam
+    em media &(dados.tempo_medio) horas conectadas.';
 
 }
 

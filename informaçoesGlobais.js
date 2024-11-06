16ab9affe4769b65ba75fa4a0f35f3611de88c5f
xaulindematadordeporco <106355661+xaulindematadordeporco@users.noreply.github.com>
2024-11-05 22:44:01 -0300

    rtb

--- informaçoesGlobais.js
+++ informaçoesGlobais.js
@@ -4,9 +4,19 @@ async function visualizarInformacoesGlobais() {
 
     const res = await fetch(url);
     const dados = await res.json();
+
+    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9).toFixed(2);
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9).toFixed(2);
+    const horas = parseInt(dados.tempo_medio);
+    const minutos = Math.round((dados.tempo_medio - horas) * 60);
+    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo) * 100).toFixed(2);
+
+
+
+
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> destas estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`;
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> destas estão conectadas em alguma rede social e passam em média <span>${horas} horas e ${minutos} minutos </span> horas conectadas. <br> Isso significa que aproximadamente <span>${porcentagemConectada} %  </span> de pessoas estão conectadas em uma rede social. `;
     
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);

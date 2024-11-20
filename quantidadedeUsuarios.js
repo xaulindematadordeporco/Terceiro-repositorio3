fbb8ee8c369adb279995f58f588435b2e2c3986e
xaulindematadordeporco <106355661+xaulindematadordeporco@users.noreply.github.com>
2024-11-19 22:51:58 -0300

    rgthjdr

--- quantidadedeUsuarios.js
+++ quantidadedeUsuarios.js
@@ -2,6 +2,21 @@ async function quantidadedeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/mais/numero-usuarios.json';
     const res = await fetch(url)
     const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadedeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadedeUsuarios,
+            type: 'bar'
+        }
+    ]
+
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico,data)
 }
 
 quantidadedeUsuarios();
